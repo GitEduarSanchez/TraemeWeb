a469e1cd94658c65c6044ef1fe2eb60ef8ad4b11
poliedro <leosanchez_19@hotmail.com>
2021-02-15 17:15:42 -0500

    inicio proyecto traemeweb

---
+++ Traeme/Traeme/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Traeme.Services;
+using Traeme.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Traeme
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
